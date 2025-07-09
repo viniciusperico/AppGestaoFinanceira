275ac4b4800bf75ec1976cd80fd074ce4ceefb18
Vinicius Périco <105397291+viniciusperico@users.noreply.github.com>
2025-07-09 15:31:29 -0300

    up

--- src/ai/genkit.ts
+++
@@ -1,8 +0,0 @@
-import { genkit } from '@genkit-ai/core';
-import { googleAI } from '@genkit-ai/googleai';
-
-// Note: The API key must be defined in an environment variable GOOGLE_API_KEY.
-// You can get a key at https://aistudio.google.com/app/apikey.
-export const ai = genkit({
-  plugins: [googleAI()],
-});
