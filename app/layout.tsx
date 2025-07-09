9715293ffebc73f3173a05f74e7f002749977158
Vinicius Périco <105397291+viniciusperico@users.noreply.github.com>
2025-07-09 19:56:38 -0300

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Lexend:wght@400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
